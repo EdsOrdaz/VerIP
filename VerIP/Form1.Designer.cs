edf39333bb7a916c907f79a2799748794f152355
EdsOrdaz <edsordaz@hotmail.com>
2022-01-26 13:35:30 -0600

    Agregar archivos de proyecto.

---
+++ VerIP/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace VerIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.iptxt = new System.Windows.Forms.TextBox();
+            this.mask = new System.Windows.Forms.TextBox();
+            this.puerta = new System.Windows.Forms.TextBox();
+            this.mactxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.adaptadortxt = new System.Windows.Forms.RichTextBox();
+            this.interfaztxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.redtxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(48, 217);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(48, 20);
+            this.label13.TabIndex = 39;
+            this.label13.Text = "MAC:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(36, 187);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(60, 20);
+            this.label12.TabIndex = 38;
+            this.label12.Text = "Puerta:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(45, 155);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "Mask:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 20);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "IP:";
+            // 
+            // iptxt
+            // 
+            this.iptxt.Enabled = false;
+            this.iptxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iptxt.Location = new System.Drawing.Point(100, 126);
+            this.iptxt.Name = "iptxt";
+            this.iptxt.ReadOnly = true;
+            this.iptxt.Size = new System.Drawing.Size(226, 26);
+            this.iptxt.TabIndex = 41;
+            // 
+            // mask
+            // 
+            this.mask.Enabled = false;
+            this.mask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mask.Location = new System.Drawing.Point(100, 155);
+            this.mask.Name = "mask";
+            this.mask.ReadOnly = true;
+            this.mask.Size = new System.Drawing.Size(226, 26);
+            this.mask.TabIndex = 42;
+            // 
+            // puerta
+            // 
+            this.puerta.Enabled = false;
+            this.puerta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puerta.Location = new System.Drawing.Point(100, 184);
+            this.puerta.Name = "puerta";
+            this.puerta.ReadOnly = true;
+            this.puerta.Size = new System.Drawing.Size(226, 26);
+            this.puerta.TabIndex = 43;
+            // 
+            // mactxt
+            // 
+            this.mactxt.Enabled = false;
+            this.mactxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mactxt.Location = new System.Drawing.Point(100, 214);
+            this.mactxt.Name = "mactxt";
+            this.mactxt.ReadOnly = true;
+            this.mactxt.Size = new System.Drawing.Size(226, 26);
+            this.mactxt.TabIndex = 44;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Adaptador:";
+            // 
+            // adaptadortxt
+            // 
+            this.adaptadortxt.Enabled = false;
+            this.adaptadortxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adaptadortxt.Location = new System.Drawing.Point(100, 12);
+            this.adaptadortxt.Name = "adaptadortxt";
+            this.adaptadortxt.Size = new System.Drawing.Size(226, 44);
+            this.adaptadortxt.TabIndex = 46;
+            this.adaptadortxt.Text = "";
+            // 
+            // interfaztxt
+            // 
+            this.interfaztxt.Enabled = false;
+            this.interfaztxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.interfaztxt.Location = new System.Drawing.Point(100, 62);
+            this.interfaztxt.Name = "interfaztxt";
+            this.interfaztxt.ReadOnly = true;
+            this.interfaztxt.Size = new System.Drawing.Size(226, 26);
+            this.interfaztxt.TabIndex = 48;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 47;
+            this.label3.Text = "Interfaz:";
+            // 
+            // redtxt
+            // 
+            this.redtxt.Enabled = false;
+            this.redtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redtxt.Location = new System.Drawing.Point(100, 94);
+            this.redtxt.Name = "redtxt";
+            this.redtxt.ReadOnly = true;
+            this.redtxt.Size = new System.Drawing.Size(226, 26);
+            this.redtxt.TabIndex = 50;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "Red:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 251);
+            this.Controls.Add(this.redtxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.interfaztxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.adaptadortxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mactxt);
+            this.Controls.Add(this.puerta);
+            this.Controls.Add(this.mask);
+            this.Controls.Add(this.iptxt);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ver IP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox iptxt;
+        private System.Windows.Forms.TextBox mask;
+        private System.Windows.Forms.TextBox puerta;
+        private System.Windows.Forms.TextBox mactxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox adaptadortxt;
+        private System.Windows.Forms.TextBox interfaztxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox redtxt;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ VerIP/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace VerIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPHostEntry host;
+            string localIP = "";
+            string mascara = "";
+            String mac = "";
+            String tipointerfaz = "";
+            String adaptador = "";
+
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+
+                    foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+                    {
+                        //Obtener mascara
+                        foreach (UnicastIPAddressInformation unicastIPAddressInformation in adapter.GetIPProperties().UnicastAddresses)
+                        {
+                            if (unicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                if (localIP.Equals(unicastIPAddressInformation.Address.ToString()))
+                                {
+                                    adaptador = adapter.Description.ToString();
+                                    tipointerfaz = adapter.NetworkInterfaceType.ToString();
+                                    mac = adapter.GetPhysicalAddress().ToString();
+
+                                    string macAddStrNew = mac;
+                                    int insertedCount = 0;
+                                    for (int i = 2; i < mac.Length; i = i + 2)
+                                    {
+                                        macAddStrNew = macAddStrNew.Insert(i + insertedCount++, ":");
+                                    }
+                                    mac = macAddStrNew;
+                                    //Console.WriteLine("MAC: " + macAddStrNew);
+                                    mascara = unicastIPAddressInformation.IPv4Mask.ToString();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+            iptxt.Text = localIP;
+            mask.Text = mascara;
+            puerta.Text = GetGateway().ToString();
+            mactxt.Text = mac;
+            adaptadortxt.Text = adaptador;
+            interfaztxt.Text = tipointerfaz;
+            redtxt.Text = red_conectada();
+        }
+
+        public static IPAddress GetGateway()
+        {
+            IPAddress result = null;
+            var cards = NetworkInterface.GetAllNetworkInterfaces().ToList();
+            if (cards.Any())
+            {
+                foreach (var card in cards)
+                {
+                    var props = card.GetIPProperties();
+                    if (props == null)
+                        continue;
+
+                    var gateways = props.GatewayAddresses;
+                    if (!gateways.Any())
+                        continue;
+
+                    var gateway =
+                        gateways.FirstOrDefault(g => g.Address.AddressFamily.ToString() == "InterNetwork");
+                    if (gateway == null)
+                        continue;
+
+                    result = gateway.Address;
+                    break;
+                };
+            }
+            if (result == null)
+            {
+                return System.Net.IPAddress.Parse("0.0.0.0");
+
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        public static String red_conectada()
+        {
+            ConnectionOptions options = new ConnectionOptions();
+            options.Impersonation = System.Management.ImpersonationLevel.Impersonate;
+
+
+            ManagementScope scope = new ManagementScope("\\\\" + Environment.MachineName.ToString() + "\\root\\StandardCimv2", options);
+            scope.Connect();
+
+            //Query system for Operating System information
+            ObjectQuery query = new ObjectQuery("SELECT * FROM MSFT_NetConnectionProfile Where IPv4Connectivity = '4'");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            ManagementObjectCollection queryCollection = searcher.Get();
+            foreach (ManagementObject m in queryCollection)
+            {
+                return m["Name"].ToString();
+            }
+            return null;
+        }
+
+    }
+}
